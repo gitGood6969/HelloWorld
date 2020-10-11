664c3623cbe3e7643629ddd328517c8fadd2e89a
gitGood6969 <syck960@uowmail.edu.au>
2020-10-11 09:30:12 +0000

    Changed Hello World

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ class HelloWorld {
     public static void main( String []args ) {
         System.out.println( "Hello World!" );
         System.out.println("Ping Pong");
+        System.out.printf("Hey this works!");
     }
 }
